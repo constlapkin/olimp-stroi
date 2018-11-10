2a92ef79131dcfe42b296c906a06856b0a5e9889
constlapkin <constlapkin@gmail.com>
2018-11-10 15:24:48 +1000

    Адаптив слайдера в бутстрапе, поправил скрипт, добавил на обе кнопки его

--- portfolio/static/js/carousel.js
+++ portfolio/static/js/carousel.js
@@ -27,60 +27,50 @@ function next() {
 
 
 window.onload = function(){
-    carousel = document.getElementById('carousel');
-    list = carousel.querySelector('ul');
+
+    list = document.getElementById('ul');
     var i = 1;
     var si = '';
     var t = [];
-    for (i; i <= (list.childNodes.length - 1) / 2; i = i + 1) {
+    for (i; i < (list.childNodes.length - 1) / 2 - 1; i = i + 1) {
         si = 'i' + i;
         t.push(document.getElementById(si));
     }
 
     var i = 1;
     var ar = 0;
-    if ((list.childNodes.length - 1) / 2 >= 4) {
+    if ((list.childNodes.length - 1) / 2 - 1 >= 4) {
         ar = 4;
     }
-    else if ((list.childNodes.length - 1) / 2 == 3) {
+    else if ((list.childNodes.length - 1) / 2 - 1 == 3) {
         ar = 3;
     }
-    else if ((list.childNodes.length - 1) / 2 == 2) {
+    else if ((list.childNodes.length - 1) / 2 - 1 == 2) {
         ar = 2;
     }
-    else if ((list.childNodes.length - 1) / 2 == 1) {
+    else if ((list.childNodes.length - 1) / 2 - 1 == 1) {
         ar = 1;
     }
-    for(i = 0; i <= ar; i++){
+    for(i = 0; i < ar; i++){
         t[i].style.display = 'block';
     }
 
 
 }
 
-
+/* TODO: Сделать проверку на разрешение экрана для перевода всех блоков в display: block */
 function fnext() {
-    // TODO: Проверить переменную i, т.к. в t элементы хранятся с нуля.
-    carousel = document.getElementById('carousel');
-    list = carousel.querySelector('ul');
+
+    list = document.getElementById('ul');
     var i = 1;
     var si = '';
     var t = [];
-    for (i; i < (list.childNodes.length - 1) / 2; i = i + 1) {
+    for (i; i < (list.childNodes.length - 1) / 2 - 1; i = i + 1) {
         si = 'i' + i;
         t.push(document.getElementById(si));
     }
     if (t.length == 8) {
-        t[1].style.display = 'none';
-        t[2].style.display = 'none';
-        t[3].style.display = 'none';
-        t[4].style.display = 'none';
-        t[5].style.display = 'block';
-        t[6].style.display = 'block';
-        t[7].style.display = 'block';
-        t[8].style.display = 'block';
-    }
-    else if (t.length == 7) {
+        t[0].style.display = 'none';
         t[1].style.display = 'none';
         t[2].style.display = 'none';
         t[3].style.display = 'none';
@@ -89,7 +79,8 @@ function fnext() {
         t[6].style.display = 'block';
         t[7].style.display = 'block';
     }
-    else if (t.length == 6) {
+    else if (t.length == 7) {
+        t[0].style.display = 'none';
         t[1].style.display = 'none';
         t[2].style.display = 'none';
         t[3].style.display = 'block';
@@ -97,16 +88,68 @@ function fnext() {
         t[5].style.display = 'block';
         t[6].style.display = 'block';
     }
-    else if (t.length == 5) {
+    else if (t.length == 6) {
+        t[0].style.display = 'none';
         t[1].style.display = 'none';
         t[2].style.display = 'block';
         t[3].style.display = 'block';
         t[4].style.display = 'block';
         t[5].style.display = 'block';
     }
-    else if(t.length < 5) {
-    /*  Убрать стрелки  */
+    else if (t.length == 5) {
+        t[0].style.display = 'none';
+        t[1].style.display = 'block';
+        t[2].style.display = 'block';
+        t[3].style.display = 'block';
+        t[4].style.display = 'block';
+    }
 
+}
+
+
+function fprev() {
+
+    list = document.getElementById('ul');
+    var i = 1;
+    var si = '';
+    var t = [];
+    for (i; i < (list.childNodes.length - 1) / 2 - 1; i = i + 1) {
+        si = 'i' + i;
+        t.push(document.getElementById(si));
+    }
+    if (t.length == 8) {
+        t[0].style.display = 'block';
+        t[1].style.display = 'block';
+        t[2].style.display = 'block';
+        t[3].style.display = 'block';
+        t[4].style.display = 'none';
+        t[5].style.display = 'none';
+        t[6].style.display = 'none';
+        t[7].style.display = 'none';
+    }
+    else if (t.length == 7) {
+        t[0].style.display = 'block';
+        t[1].style.display = 'block';
+        t[2].style.display = 'block';
+        t[3].style.display = 'block';
+        t[4].style.display = 'none';
+        t[5].style.display = 'none';
+        t[6].style.display = 'none';
+    }
+    else if (t.length == 6) {
+        t[0].style.display = 'block';
+        t[1].style.display = 'block';
+        t[2].style.display = 'block';
+        t[3].style.display = 'block';
+        t[4].style.display = 'none';
+        t[5].style.display = 'none';
+    }
+    else if (t.length == 5) {
+        t[0].style.display = 'block';
+        t[1].style.display = 'block';
+        t[2].style.display = 'block';
+        t[3].style.display = 'block';
+        t[4].style.display = 'none';
     }
 
 }
